f1938f2ed11667675d5bd715766758782a1df3f8
kchangela7 <kchangela7@gmail.com>
2020-09-28 12:10:06 -0400

    changed fonts

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: "Lato"
       ),
       home: Home(),
     );
